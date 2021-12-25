3e0918b79441e762003b150da8af97512e713a96
Unzor <71789103+Unzor@users.noreply.github.com>
2021-12-25 00:11:56 -0600

    Update first-letter-uppercase.js

--- first-letter-uppercase.js
+++ first-letter-uppercase.js
@@ -1,4 +1,4 @@
-String.prototype.firstLetterMayuscule = function() {
+String.prototype.firstLetterUppercase = function() {
 var spliced = this.split('');
 var spliced2 = spliced.slice(1, spliced.length);
 var spliced3 = [spliced[0].toUpperCase(), spliced2.join("")]
